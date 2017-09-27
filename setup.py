f1ea69db4d5e4ec6a51358e84513dc265aa3161a
Joel Tuberosa <joel.tuberosa@unige.ch>
2017-09-27 19:04:11 +0200

    add classifiers

--- setup.py
+++ setup.py
@@ -15,6 +15,18 @@ setup(
     scripts=['script/psgfinder.py'],
     packages=['psgfindertools'],
     platforms=['any'],
-    version='2.5',
+    version='2.5.b1',
     long_description=readme(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        ]
       )
