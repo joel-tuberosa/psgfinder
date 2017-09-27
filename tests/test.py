3bc54e30abdb6b23923cfb01afd99e87aa5eaa5d
Joel Tuberosa <joel.tuberosa@unige.ch>
2017-09-27 23:58:03 +0200

    started writing tests/test.py

---
+++ tests/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import unittest
+
+class PSGfinderTest(unittest.TestCase):
+    
+    # handle an alignment from a file
+    def test_handle_fasta(self):
+    
+    # translate a sequence
+    def test_translate
+    
+    # find amino acid differences in an coding sequence alignment
+    def test_aadiff(self)
+    
+    # return windows from an alignment 
